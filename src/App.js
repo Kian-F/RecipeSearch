d767190d81cb06b77460baf82aed30d50219cf50
Kian-F <52603628+Kian-F@users.noreply.github.com>
2020-10-30 15:58:50 +1100

    env

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import { render } from '@testing-library/react';
 import React from 'react';
+import Api from './components/Api'
 import './App.css';
+require('dotenv').config();
 
 function App() {
   
   return (
     <div className="App">
+      <Api/>
+      <form className='search-form'>
+        <input className='search-bar' type="text"/>
+        <button className='search-button' type='submit'></button>
+      </form>
     
     </div>
   );
--- src/components/Api.js
+++ src/components/Api.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 
-const API_ID = process.env.API_ID
-const API_KEY = process.env.API_KEY
 
-const Api = () =>{
 
-}
+const Api = () => {
+   
+    
+  const Api_id = process.env.REACT_APP_API_ID;
+  const Api_key = process.env.REACT_APP_API_KEY;
+
+  console.log(process.env.REACT_APP_API_ID)
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${Api_id}&app_key=${Api_key}`
+    );
+    const data =await response.json();
+    console.log(data);
+  };
+
+  return(
+      <h1>API</h1>
+  )
+};
 
 export default Api;
