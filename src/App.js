0c54159eef5085ea0a723f22407d2ccc8c3e9ab2
Kian-F <52603628+Kian-F@users.noreply.github.com>
2020-11-11 01:06:59 +1100

    query

--- src/App.js
+++ src/App.js
@@ -5,23 +5,29 @@ import "./App.css";
 
 function App() {
   const [search, setSearch] = useState("");
-  const getSearchQuery = (e) => {
+  const [query, setQuery] = useState("")
+  const getInput = (e) => {
     setSearch(e.target.value);
     console.log(search);
   };
+  const setInput = (e)=>{
+    e.preventDefault();
+    setQuery(search)
+    setSearch("")
+  }
   return (
     <div className="App">
-      <form className="search-form">
+      <form className="search-form" onSubmit={setInput}>
         <input
           className="search-bar"
           type="text"
           value={search}
-          onChange={getSearchQuery}
+          onChange={getInput}
         />
         <button className="search-button" type="submit">
           Search
         </button>
-        <Api />
+        <Api query={query}/>
       </form>
     </div>
   );
--- src/components/Api.js
+++ src/components/Api.js
@@ -2,15 +2,16 @@ import React, { useEffect, useState } from "react";
 import Recipe from "./containers/Recipe";
 
 const Api = (props) => {
+  console.log(props)
   const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
     fetchRecipes();
-  }, []);
+  }, [props.query]);
 
   const fetchRecipes = async () => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}`
+      `https://api.edamam.com/search?q=${props.query}&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}`
     );
     const data = await response.json();
     setRecipes(data.hits);
